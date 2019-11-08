5b253e73f3e53ef41bb5fbcf6f3dd87d2987128b
Chauncey Wang <chncwang@gmail.com>
2019-11-08 14:01:31 +0800

    fix perplex problem

--- src/single_turn_conversation/perplex.h
+++ src/single_turn_conversation/perplex.h
@@ -16,10 +16,7 @@ dtype computePerplex(const std::vector<Node *> &nodes, const std::vector<int> &a
 
     for (int i = 0; i < nodes.size(); ++i) {
         Node &node = *nodes.at(i);
-        auto exped_result = toExp(node);
-        n3ldg_cpu::Tensor1D &t = *std::get<0>(exped_result).get();
-        dtype sum = std::get<2>(exped_result);
-        dtype reciprocal_answer_prob = sum / t.v[answers.at(i)];
+        dtype reciprocal_answer_prob = 1 / node.getVal().v[answers.at(i)];
         log_sum += log(reciprocal_answer_prob);
     }
 
