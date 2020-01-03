73fcb11ebe01b0f3e1e42d3edee4b735dea131c7
Chauncey Wang <chncwang@gmail.com>
2020-01-03 13:59:20 +0800

    keyword dropout

--- src/single_turn_conversation/encoder_decoder/decoder_components.h
+++ src/single_turn_conversation/encoder_decoder/decoder_components.h
@@ -14,7 +14,7 @@ struct ResultAndKeywordVectors {
 struct DecoderComponents {
     std::vector<LookupNode<Param> *> decoder_lookups_before_dropout;
     std::vector<DropoutNode *> decoder_lookups;
-    std::vector<LookupNode<Param> *> decoder_keyword_lookups;
+    std::vector<Node *> decoder_keyword_lookups;
     std::vector<Node *> decoder_to_wordvectors;
     std::vector<Node *> decoder_to_keyword_vectors;
     std::vector<Node *> wordvector_to_onehots;
