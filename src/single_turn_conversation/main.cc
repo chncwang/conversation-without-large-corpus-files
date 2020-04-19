dc63ce775ba26d1239e7a5efe68b930ca050844c
Chauncey Wang <chncwang@gmail.com>
2020-04-19 22:02:43 +0800

    matched idf

--- src/single_turn_conversation/main.cc
+++ src/single_turn_conversation/main.cc
@@ -509,6 +509,8 @@ void decodeTestPosts(const HyperParams &hyper_params, ModelParams &model_params,
         }
         float idf_value = computeEntropy(candidate_and_references_vector, all_idf);
         cout << "idf:" << idf_value << endl;
+        float matched_idf = computeMatchedEntropy(candidate_and_references_vector, all_idf);
+        cout << "matched idf:" << matched_idf << endl;
         float greedy_matching_sim = computeGreedyMatching(candidate_and_references,
                 model_params.lookup_table);
         greedy_matching_similarities.push_back(greedy_matching_sim);
