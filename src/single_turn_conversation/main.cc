75c9132fb0437afd96b040c86d82827243cf0532
Chauncey Wang <chncwang@gmail.com>
2020-05-16 16:06:31 +0800

    merge

--- src/single_turn_conversation/main.cc
+++ src/single_turn_conversation/main.cc
@@ -422,12 +422,13 @@ void decodeTestPosts(const HyperParams &hyper_params, ModelParams &model_params,
     vector<CandidateAndReferences> candidate_and_references_vector;
     map<string, int64_t> overall_flops;
     float mmi_flops_sum = 0;
+    int64_t activations_sum = 0;
     int loop_i = 0;
     for (const PostAndResponses &post_and_responses : post_and_responses_vector) {
         ++loop_i;
         cout << "post:" << endl;
         print(post_sentences.at(post_and_responses.post_id));
-        Graph graph(false, true);
+        Graph graph(false, true, true);
         GraphBuilder graph_builder;
         graph_builder.forward(graph, post_sentences.at(post_and_responses.post_id), hyper_params,
                 model_params, false);
@@ -465,6 +466,10 @@ void decodeTestPosts(const HyperParams &hyper_params, ModelParams &model_params,
 
         cout << boost::format("flops overall:%1% avg:%2%") % flops_sum %
             (static_cast<float>(flops_sum) / loop_i) << endl;
+
+        activations_sum += graph.getActivations();
+        cout << boost::format("activations:%1% avg:%2%") % activations_sum %
+            (static_cast<float>(activations_sum) / loop_i) << endl;
 
         dtype probability = pair.second;
         cout << format("probability:%1%") % probability << endl;
